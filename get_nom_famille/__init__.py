e4c1a9a5e3297beff131a3c94e02d810166dd758
Kenza-sh <kenza@goodman-ai.com>
2025-04-08 17:28:33 +0200

    Update __init__.py

--- get_nom_famille/__init__.py
+++ get_nom_famille/__init__.py
@@ -42,7 +42,8 @@ class InformationExtractor:
         logger.warning("Aucun nom n'a été extrait.")
         return None
       
-  extractor = InformationExtractor(nlp)
+    
+extractor = InformationExtractor(nlp)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
